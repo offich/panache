206911bb95f3e911a31358e427e82577b98fc6ce
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-01-09 17:56:25 +0900

    feat: define unit enum

---
+++ lib/src/model/unit.dart
@@ -0,0 +1,38 @@
+enum Unit {
+  paragraphs,
+  sentences,
+  words,
+  bytes,
+}
+
+extension UnitProps on Unit {
+  double get max {
+    switch (this) {
+      case Unit.paragraphs:
+        return 5.0;
+      case Unit.sentences:
+        return 20.0;
+      case Unit.words:
+        return 100.0;
+      case Unit.bytes:
+        return 500.0;
+    }
+  }
+
+  double get interval {
+    return max / 5;
+  }
+
+  String get text {
+    switch (this) {
+      case Unit.paragraphs:
+        return 'Paragraphs';
+      case Unit.sentences:
+        return 'Sentences';
+      case Unit.words:
+        return 'Words';
+      case Unit.bytes:
+        return 'Bytes';
+    }
+  }
+}
