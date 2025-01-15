24c68690577f60cd08cde22ed14a6dc790065d96
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-01-15 10:25:25 +0900

    fix: add font size

--- lib/src/ui/style/text.dart
+++ lib/src/ui/style/text.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class PanacheTextStyle {
-  static const TextStyle large = TextStyle(fontSize: 16, height: 26 / 14);
+  static const TextStyle large = TextStyle(fontSize: 20, height: 26 / 14);
   static const TextStyle largeBold = TextStyle(
-    fontSize: 16,
+    fontSize: 20,
     height: 26 / 14,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle medium = TextStyle(fontSize: 14, height: 22 / 14);
+  static const TextStyle medium = TextStyle(fontSize: 16, height: 22 / 14);
   static const TextStyle mediumBold = TextStyle(
-    fontSize: 14,
+    fontSize: 16,
     height: 22 / 14,
     fontWeight: FontWeight.bold,
   );
