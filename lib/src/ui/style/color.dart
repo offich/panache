41263bd69dba57619eea58e1bbb73d540307ff97
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-01-15 18:11:24 +0900

    feat: define color

---
+++ lib/src/ui/style/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PanacheColor {
+  static const Color primaryColor = Color.fromRGBO(244, 232, 105, 1.0);
+  static const Color secondaryColor = Color.fromRGBO(250, 242, 211, 1.0);
+  static const Color thirdlyColor = Color.fromRGBO(92, 210, 230, 1.0);
+
+  static Color errorColor = Colors.red[300]!;
+}
