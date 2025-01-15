ff5823e88eb20c4d6cf6c11cd1835914079c2d2d
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-01-15 11:51:13 +0900

    feat: add fade in and slide transition

--- lib/src/ui/components/lorem_text.dart
+++ lib/src/ui/components/lorem_text.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class LoremText extends StatelessWidget {
+class LoremText extends HookWidget {
   final String text;
 
   const LoremText({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Text(text);
+    final animationController = useAnimationController(
+      duration: const Duration(milliseconds: 500),
+    );
+    final opacityAnimation = useState<Animation<double>?>(null);
+    final offsetAnimation = useState<Animation<Offset>?>(null);
+
+    useEffect(() {
+      opacityAnimation.value = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeIn,
+        ),
+      );
+
+      offsetAnimation.value =
+          Tween<Offset>(begin: const Offset(0, 1), end: Offset.zero).animate(
+        CurvedAnimation(
+          parent: animationController,
+          curve: Curves.easeInOut,
+        ),
+      );
+
+      animationController.forward();
+
+      return;
+    }, []);
+
+    if (offsetAnimation.value == null || opacityAnimation.value == null) {
+      return SizedBox.shrink();
+    }
+
+    return SlideTransition(
+      position: offsetAnimation.value!,
+      child: FadeTransition(
+        opacity: opacityAnimation.value!,
+        child: Text(text),
+      ),
+    );
   }
 }
