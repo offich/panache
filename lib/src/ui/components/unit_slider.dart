b9d4bba7c5a6d7636f50c88e6146387cc5baf119
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-01-16 10:48:55 +0900

    feat: display focus ring

--- lib/src/ui/components/unit_slider.dart
+++ lib/src/ui/components/unit_slider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:panache/src/model/unit.dart';
 import 'package:panache/src/ui/style/color.dart';
 import 'package:syncfusion_flutter_core/theme.dart';
 import 'package:syncfusion_flutter_sliders/sliders.dart';
 
-class UnitSlider extends StatelessWidget {
+class UnitSlider extends HookWidget {
   final Unit unit;
   final int unitNum;
   final Function(int) onChanged;
@@ -18,31 +19,49 @@ class UnitSlider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SfSliderTheme(
-      data: SfSliderThemeData(
-        activeTrackColor:
-            unitNum > 0 ? PanacheColor.secondaryColor : PanacheColor.errorColor,
-        inactiveTrackColor:
-            unitNum > 0 ? PanacheColor.secondaryColor : PanacheColor.errorColor,
-        activeTickColor: PanacheColor.secondaryColor,
-        inactiveTickColor: PanacheColor.secondaryColor,
-        thumbColor: PanacheColor.primaryColor,
-        tooltipBackgroundColor: PanacheColor.primaryColor,
-        tooltipTextStyle: TextStyle(color: Colors.black),
-        overlayColor: PanacheColor.primaryColor.withValues(alpha: 0.1),
-        overlayRadius: 20.0,
-      ),
-      child: SfSlider(
-        min: 0.0,
-        max: unit.max,
-        interval: unit.interval,
-        showTicks: true,
-        showLabels: true,
-        enableTooltip: true,
-        value: unitNum,
-        onChanged: (value) {
-          onChanged(value.floor());
-        },
+    final focusNode = useFocusNode();
+    final isFocused = useState(false);
+
+    useEffect(() {
+      focusNode.addListener(() {
+        isFocused.value = focusNode.hasFocus;
+      });
+
+      return;
+    }, []);
+
+    return Focus(
+      focusNode: focusNode,
+      child: SfSliderTheme(
+        data: SfSliderThemeData(
+          activeTrackColor: unitNum > 0
+              ? PanacheColor.secondaryColor
+              : PanacheColor.errorColor,
+          inactiveTrackColor: unitNum > 0
+              ? PanacheColor.secondaryColor
+              : PanacheColor.errorColor,
+          activeTickColor: PanacheColor.secondaryColor,
+          inactiveTickColor: PanacheColor.secondaryColor,
+          thumbColor: PanacheColor.primaryColor,
+          thumbStrokeColor: isFocused.value ? PanacheColor.thirdlyColor : null,
+          thumbStrokeWidth: isFocused.value ? 2 : null,
+          tooltipBackgroundColor: PanacheColor.primaryColor,
+          tooltipTextStyle: TextStyle(color: Colors.black),
+          overlayColor: PanacheColor.primaryColor.withValues(alpha: 0.1),
+          overlayRadius: 20.0,
+        ),
+        child: SfSlider(
+          min: 0.0,
+          max: unit.max,
+          interval: unit.interval,
+          showTicks: true,
+          showLabels: true,
+          enableTooltip: true,
+          value: unitNum,
+          onChanged: (value) {
+            onChanged(value.floor());
+          },
+        ),
       ),
     );
   }
