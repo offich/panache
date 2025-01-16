d4e5a5532b06ff0c149197b869cd3e39d2658184
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-01-16 10:35:48 +0900

    fix: remove focus on clicked outside

--- lib/panache.dart
+++ lib/panache.dart
@@ -14,80 +14,86 @@ class Panache extends HookWidget {
   Widget build(BuildContext context) {
     final generated = useState(Lorem().generate());
 
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 16.0,
-            children: [
-              SizedBox(
-                width: 280,
-                child: Sidebar(onNumsChanged: ({
-                  required paragraphs,
-                  required sentences,
-                  required words,
-                }) {
-                  generated.value = Lorem().generate(
-                    paragraphs: paragraphs,
-                    sentences: sentences,
-                    words: words,
-                  );
-                }),
-              ),
-              VerticalDivider(
-                thickness: 4,
-                color: PanacheColor.primaryColor,
-              ),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [AnimatedText(text: generated.value)],
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16.0,
+              children: [
+                SizedBox(
+                  width: 280,
+                  child: Sidebar(onNumsChanged: ({
+                    required paragraphs,
+                    required sentences,
+                    required words,
+                  }) {
+                    generated.value = Lorem().generate(
+                      paragraphs: paragraphs,
+                      sentences: sentences,
+                      words: words,
+                    );
+                  }),
+                ),
+                VerticalDivider(
+                  thickness: 4,
+                  color: PanacheColor.primaryColor,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [AnimatedText(text: generated.value)],
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 16.0,
-          right: 16.0,
-          child: SizedBox(
-            width: 48,
-            height: 48,
-            child: IconButton(
-              style: IconButton.styleFrom(
-                foregroundColor: PanacheColor.secondaryColor,
-                backgroundColor: PanacheColor.thirdlyColor,
-              ),
-              padding: EdgeInsets.all(0.0),
-              icon: const Icon(Icons.copy),
-              onPressed: () async {
-                final snackBar = SnackBar(
-                  backgroundColor: PanacheColor.primaryColor,
-                  width: 360.0,
-                  content: Text(
-                    "Copied Text!!",
-                    style:
-                        PanacheTextStyle.medium.copyWith(color: Colors.black),
-                  ),
-                  duration: const Duration(seconds: 2),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  behavior: SnackBarBehavior.floating,
-                );
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-                await Clipboard.setData(ClipboardData(text: generated.value));
-              },
+              ],
             ),
           ),
-        )
-      ],
+          Positioned(
+            bottom: 16.0,
+            right: 16.0,
+            child: SizedBox(
+              width: 48,
+              height: 48,
+              child: IconButton(
+                style: IconButton.styleFrom(
+                  foregroundColor: PanacheColor.secondaryColor,
+                  backgroundColor: PanacheColor.thirdlyColor,
+                ),
+                padding: EdgeInsets.all(0.0),
+                icon: const Icon(Icons.copy),
+                onPressed: () async {
+                  final snackBar = SnackBar(
+                    backgroundColor: PanacheColor.primaryColor,
+                    width: 360.0,
+                    content: Text(
+                      "Copied Text!!",
+                      style:
+                          PanacheTextStyle.medium.copyWith(color: Colors.black),
+                    ),
+                    duration: const Duration(seconds: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    behavior: SnackBarBehavior.floating,
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                  await Clipboard.setData(ClipboardData(text: generated.value));
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
