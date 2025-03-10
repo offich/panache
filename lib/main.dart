346fc1e517459257b02ecfbe31bc68ca54e1868e
ume-kun1015 <baymax.blackandwhite@gmail.com>
2025-03-10 11:35:36 +0900

    chore: leave comment to provide doc

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:panache/panache.dart';
 
+/// Runs Panache on DevToolsExtension
 void main() {
   runApp(DevToolsExtension(child: const Panache()));
 }
